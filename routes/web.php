619ab438165e31d0bb7ff99f1b699bd6214c1e11
Luiz Silva <luizsilva.ee@gmail.com>
2022-12-18 20:43:42 -0300

    feat(routes): relatório de requisições por consumidor

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LogController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
+});
+
+Route::prefix('/report')->group(function(){
+    Route::get('/consumer', [LogController::class, 'exportLogsByConsumerId'])->name('consumer');
 });
