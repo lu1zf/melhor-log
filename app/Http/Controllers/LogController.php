5dc4b01d57d40970b77010a429cfe8ceb6ab7b1a
Luiz Silva <luizsilva.ee@gmail.com>
2022-12-18 23:29:31 -0300

    feat(reports): adiciona download de relatórios

--- app/Http/Controllers/LogController.php
+++ app/Http/Controllers/LogController.php
@@ -4,27 +4,40 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Services\LogService;
+use App\Services\LogReportService;
+
 class LogController extends Controller
 {
     private LogService $logService;
+    private LogReportService $logReportService;
 
     public function __construct()
     {
         $this->logService = new LogService();
+        $this->logReportService = new LogReportService($this->logService);
     }
 
     public function exportLogsByConsumerId()
     {
-        dd($this->logService->getRequestsByConsumerId());
+        $this->logReportService->exportRequestsByConsumerId();
+
+        $filePath = storage_path('consumerReport.csv');
+        return response()->download($filePath);
     }
 
     public function exportLogsByServiceId()
     {
-        dd($this->logService->getRequestsByServiceId());
+        $this->logReportService->exportRequestsByServiceId();
+
+        $filePath = storage_path('serviceReport.csv');
+        return response()->download($filePath);
     }
 
     public function exportAverageLatenciesByService()
     {
-        dd($this->logService->getAverageLatenciesByService());
+        $this->logReportService->exportAverageLatenciesReport();
+
+        $filePath = storage_path('avgLatenciesReport.csv');
+        return response()->download($filePath);
     }
 }
